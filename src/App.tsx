edad9c4911e98705c3cf19ce9b2000113da34ab4
Gauthier Dumon <dumon.gauthier@gmail.com>
2025-11-20 12:08:53 +0100

    Mon premier commit personnel

--- src/App.tsx
+++ src/App.tsx
@@ -37,3 +37,4 @@ const App = () => (
 export default App;
 // Ceci est un commentaire pour créer une modification à commiter.
 // Nouveau commentaire pour le commit utilisateur.
+// Ceci est ma propre modification !
